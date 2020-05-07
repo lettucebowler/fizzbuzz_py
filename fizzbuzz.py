f135bbcc583fb39e242926ce2c811222f806006b
Lettucebowler <lettucebowler@gmail.com>
2020-05-07 18:46:50 -0500

    Uses list comprehension now because I like that stuff. Also ends iteration at lcm of keys in words dictionary

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 from math import gcd
+from random import choice
 
 words = {3: 'fizz', 5: 'buzz'}
 
-for n in range(1, 100):
-    output = ""
-    for word in words:
-        if n % word == 0:
-            output += words[word]
-    if output == "":
-        print(str(n))
-    else:
-        print(str(output))
+# Calculate lcm of numbers to check. Used to determine length of pattern.
+keys = list(words.keys())
+lcm = keys[0]
+for i in keys[1:]:
+    lcm = int(lcm*i/gcd(lcm, i))
+
+for n in range(1, lcm + 1):
+    output = [words[word] for word in words if n % word == 0]
+    print("".join(output) if len(output) > 0 else n)
