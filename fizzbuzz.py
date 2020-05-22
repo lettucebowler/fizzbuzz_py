43d84f29b02b36e97ea88230ad9de7180a41e25a
Grant Montgomery <lettucebowler@gmail.com>
2020-05-21 23:08:55 -0500

    added global variables for function operation

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,18 +1,41 @@
 #!/usr/bin/env python3
 from math import gcd
 
+# Global behavior variables
+MOD = True
+LESS = False
+
+
 # Add entries here to check more numbers
 mod_dict = {3: 'Fizz', 5: 'Buzz'}
 less_dict = {10: 'Bazz'}
 
-# Calculate lcm of numbers to check. Used to determine length of pattern.
-keys = list(words.keys())
-lcm = keys[0]
-for i in keys[1:]:
-    lcm = int(lcm*i/gcd(lcm, i))
 
-for n in range(1, lcm + 1):
+range_list = []
+
+# Calculate lcm of numbers to check. Used to determine length of pattern.
+if MOD and len(mod_dict) > 0:
+    keys = list(mod_dict.keys())
+    lcm = keys[0]
+    for i in keys[1:]:
+        lcm = int(lcm*i/gcd(lcm, i))
+    range_list.append(lcm)
+
+#find largest value checked for less_dict
+if LESS and len(less_dict) > 0:
+    range_list.append(max(list(less_dict.keys())))
+
+# Determine largest checked value and set range of iteration to it.
+if len(range_list) > 0:
+    final_num = max(range_list)
+else:
+    print("No Functions Enabled.")
+    exit()
+
+for n in range(1, final_num + 1):
     output = []
-    output.extend([less_dict[less] for less in less_dict if n < less])
-    output.extend([mod_dict[word] for word in mod_dict if n % word == 0])
+    if MOD:
+        output.extend([mod_dict[word] for word in mod_dict if n % word == 0])
+    if LESS:
+        output.extend([less_dict[less] for less in less_dict if n < less])
     print("".join(output) if len(output) > 0 else n)
