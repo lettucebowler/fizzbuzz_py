21bdb98c83d5527fb5042d70e1a5c584a3580bb8
Grant Montgomery <lettucebowler@gmail.com>
2020-05-21 22:48:44 -0500

    added functionality to check if n is less than a number

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,8 @@
 from math import gcd
 
 # Add entries here to check more numbers
-words = {3: 'fizz', 5: 'buzz'}
+mod_dict = {3: 'Fizz', 5: 'Buzz'}
+less_dict = {10: 'Bazz'}
 
 # Calculate lcm of numbers to check. Used to determine length of pattern.
 keys = list(words.keys())
@@ -11,5 +12,7 @@ for i in keys[1:]:
     lcm = int(lcm*i/gcd(lcm, i))
 
 for n in range(1, lcm + 1):
-    output = [words[word] for word in words if n % word == 0]
+    output = []
+    output.extend([less_dict[less] for less in less_dict if n < less])
+    output.extend([mod_dict[word] for word in mod_dict if n % word == 0])
     print("".join(output) if len(output) > 0 else n)
