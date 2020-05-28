ceacf2bb84b0ebe5cb83590eeaa447541f90338e
Grant Montgomery <lettucebowler@gmail.com>
2020-05-28 13:07:11 -0500

    moved fizzbuzz logic to a function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 from math import gcd
 
+# n: number to check
+# states: dictionary of form number:word containing numbers to check against n
+# output: list of conditions met, or [n] if none
+def fizzbuzz(n, states):
+    output = [str(n)]
+    output.extend([states[k] for k in states if n % k == 0])
+    if len(output) > 1:
+        output = output[1:]
+    return output
+
 # Add entries here to check more numbers
-mod_dict = {3: 'Fizz', 5: 'Buzz'}
+conditions = {3: 'Fizz', 5: 'Buzz'}
 
 # Calculate lcm of numbers to check. Used to determine length of pattern.
-if len(mod_dict) > 0:
-    keys = list(mod_dict.keys())
+if len(conditions) > 0:
+    keys = list(conditions.keys())
     lcm = keys[0]
     for i in keys[1:]:
         lcm = int(lcm*i/gcd(lcm, i))
 
+# Run program until all possible conditions have been met
 for n in range(1, lcm + 1):
-    output = []
-    output.extend([mod_dict[word] for word in mod_dict if n % word == 0])
-    print("".join(output) if len(output) > 0 else n)
+    print(''.join(fizzbuzz(n, conditions)))
