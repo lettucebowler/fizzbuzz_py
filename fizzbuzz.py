3dc1e5b405ca7a8bf8cec59061ad10021c041af3
Grant Montgomery <lettucebowler@gmail.com>
2020-05-28 13:28:39 -0500

    reformatted for loop to list comprehension

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,19 +6,21 @@ from math import gcd
 # output: list of conditions met, or [n] if none
 def fizzbuzz(n, states):
     output = [str(n)]
-    output.extend([states[k] for k in states if n % k == 0])
+    output.extend([text for divisor, text in states if n % divisor == 0])
+
     if len(output) > 1:
         output = output[1:]
     return output
 
 # Add entries here to check more numbers
-conditions = {3: 'Fizz', 5: 'Buzz'}
+# conditions = {3: 'Fizz', 5: 'Buzz'}
+conditions = [(3, 'Fizz'), (5, 'Buzz')]
 
 # Calculate lcm of numbers to check. Used to determine length of pattern.
 if len(conditions) > 0:
-    keys = list(conditions.keys())
-    lcm = keys[0]
-    for i in keys[1:]:
+    divisors = [divisor for divisor, text in conditions]
+    lcm = divisors[0]
+    for i in divisors[1:]:
         lcm = int(lcm*i/gcd(lcm, i))
 
 # Run program until all possible conditions have been met
